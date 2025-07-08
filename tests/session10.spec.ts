4fbe999bccbcd0d2b68dcb2cc8be12dc3b92e2b4
ROSEMARIE_ROLDAN <roldan_rosemarie24@yahoo.com>
2025-07-08 20:26:13 +0800

    update for   session 10 act

--- tests/session10.spec.ts
+++ tests/session10.spec.ts
@@ -12,16 +12,27 @@ test.describe('ParaBank ▶ Registration page', () => {
   test('Successful registration with valid data', async ({ page }) => {
     // Fill in valid data
   
-  await page.locator('[id="customer.firstName"]').fill('a');
+  await page.locator('[id="customer.firstName"]').click();
+  let name = await page.locator('[id="customer.firstName"]').fill('a');
+  await page.locator('[id="customer.lastName"]').click();
   await page.locator('[id="customer.lastName"]').fill('a');
+  await page.locator('[id="customer.address.street"]').click();
   await page.locator('[id="customer.address.street"]').fill('a');
+  await page.locator('[id="customer.address.city"]').click();
   await page.locator('[id="customer.address.city"]').fill('a');
+  await page.locator('[id="customer.address.state"]').click();
   await page.locator('[id="customer.address.state"]').fill('a');
+  await page.locator('[id="customer.address.zipCode"]').click();
   await page.locator('[id="customer.address.zipCode"]').fill('a');
+  await page.locator('[id="customer.phoneNumber"]').click();
   await page.locator('[id="customer.phoneNumber"]').fill('a');
+  await page.locator('[id="customer.ssn"]').click();
   await page.locator('[id="customer.ssn"]').fill('a');
+  await page.locator('[id="customer.username"]').click();
   await page.locator('[id="customer.username"]').fill('a');
+  await page.locator('[id="customer.password"]').click();
   await page.locator('[id="customer.password"]').fill('a');
+  await page.locator('#repeatedPassword').click();
   await page.locator('#repeatedPassword').fill('a');
  
   
@@ -30,7 +41,6 @@ test.describe('ParaBank ▶ Registration page', () => {
     await page.getByRole('button', { name: 'Register' }).click();
 
     // Expect redirection and confirmation
-    let name = await page.getByLabel('First Name').fill('a');
     await expect(page).toHaveURL('https://parabank.parasoft.com/parabank/register.htm');
     await expect(page.locator('.title')).toContainText(`Welcome ${name}`);
   });
